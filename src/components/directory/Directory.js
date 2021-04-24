6be7b1e33179053d601580fed59bcd695d6efdc8
Rahul Patel <patel.r03@gmail.com>
2021-04-23 21:59:04 -0400

    updated menu-items to navigate withRouter HOC

---
+++ src/components/directory/Directory.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+
+import MenuItem from '../menu-item/MenuItem';
+
+import './Directory.styles.scss';
+
+class Directory extends Component {
+    constructor() {
+        super();
+        
+        this.state = {
+            sections: [
+                {
+                  title: 'HATS',
+                  imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                  id: 1,
+                  linkUrl: 'hats'
+                },
+                {
+                  title: 'JACKETS',
+                  imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+                  id: 2,
+                  linkUrl: ''
+                },
+                {
+                  title: 'SNEAKERS',
+                  imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+                  id: 3,
+                  linkUrl: ''
+                },
+                {
+                  title: 'WOMENS',
+                  imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+                  size: 'large',
+                  id: 4,
+                  linkUrl: ''
+                },
+                {
+                  title: 'MENS',
+                  imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+                  size: 'large',
+                  id: 5,
+                  linkUrl: ''
+                }
+              ]
+        }
+    }
+
+    render() {
+      return (
+        <div className='directory-menu'>
+          {
+            this.state.sections.map(({id, ...otherSectionProps}) => (
+              <MenuItem key={id} {...otherSectionProps} />
+            ))
+          }
+        </div>
+      )
+    }
+};
+
+export default Directory;
