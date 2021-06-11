42e8d25bc6cfb0973946386b2b5351a4e3bf0067
Rahul Patel <patel.r03@gmail.com>
2021-06-10 20:04:28 -0400

    Storing user data in app from firestore db

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Header from './components/header/Header';
 import SignInandSignUp from './pages/sign-in-and-signup/SignInandSignUp';
 
 import './App.css';
-import { auth } from './firebase/firebase.utility';
+import { auth, createUserProfileDocument } from './firebase/firebase.utility';
 
 class App extends React.Component{
   constructor(){
@@ -21,10 +21,24 @@ class App extends React.Component{
   unsubscribeFromAuth = null;
 
   componentDidMount(){
-    this.unsubscribeFromAuthauth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user});
+    this.unsubscribeFromAuth = auth.onAuthStateChanged( async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          }, 
+          () => {
+            console.log(this.state);
+          })
+        });
+      }
 
-      console.log(user);
+      this.setState({currentUser: userAuth});
     });
   }
 
